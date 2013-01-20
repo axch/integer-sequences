13612fb7d561ba381248170a024f8033d975dfe6
Alexey Radul <axch@mit.edu>
2013-01-20 08:51:06 -0500

    Load file.

---
+++ load.scm
@@ -0,0 +1,36 @@
+;;; This file is part of Integer Sequences, a library for recreational
+;;; number theory in MIT Scheme.
+;;; Copyright 2007-2009 Alexey Radul.
+;;;
+;;; Integer Sequences is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Affero General Public License
+;;; as published by the Free Software Foundation; either version 3 of
+;;; the License, or (at your option) any later version.
+;;; 
+;;; This code is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU Affero General Public
+;;; License along with Integer Sequences; if not, see
+;;; <http://www.gnu.org/licenses/>.
+
+;;;; Loading Integer Sequences
+
+(define (self-relatively thunk)
+  (let ((place (ignore-errors current-load-pathname)))
+    (if (pathname? place)
+	(with-working-directory-pathname
+	 (directory-namestring place)
+	 thunk)
+	(thunk))))
+
+(define (load-relative filename)
+  (self-relatively (lambda () (load filename))))
+
+(load-relative "support/auto-compilation")
+(load-relative-compiled "support/srfi-45")
+(load-relative-compiled "support/streams")
+(load-relative-compiled "numbers-meta")
+(load-relative-compiled "numbers")
